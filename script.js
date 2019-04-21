5958053694a6525aeb0b3bf91b1a673f5e15b16c
Eugen Str <asng99bk@yandex.ru>
2019-04-22 01:51:13 +0400

    Added save games by means of local storage

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.onload = (function() {
-	var ingredients = document.querySelectorAll('.ingredient-item'),
+	let ingredients = document.querySelectorAll('.ingredient-item'),
 		ingredientsUl = document.querySelectorAll('.ingredient-items'),
 		craftfield = document.querySelector('.alch-circle'),
 		button =  document.querySelector('.craft-new-item'),
 		craft = document.querySelector('.craft__field-for-items'),
 		count = 0,
-		count2 = ['Water', 'Earth', 'Air', 'Fire'];
+		count2 = ['Water', 'Earth', 'Air', 'Fire'],
+		fromJson = [],
+		reset = document.querySelector('.reset');
 
 	for (let i = 0; i < ingredients.length; i++) {
 		ingredients[i].addEventListener('click', function addClick(e = event) {
@@ -22,6 +24,60 @@ window.onload = (function() {
 		})
 	}
 
+	reset.addEventListener('click', function() {
+		localStorage.clear();
+		window.location.reload();
+	})
+		
+		if (localStorage.length !== 0 ) {
+			fromJson = JSON.parse(localStorage.getItem('array'));
+			const takeUl = document.querySelectorAll('.ingredient-items');
+			for (let i = 4; i < fromJson.length; i++) {
+				const item = localStorage.getItem(fromJson[i]),
+					  createLi = document.createElement('li');
+				createLi.setAttribute('id', fromJson[i]);
+				createLi.innerHTML = `<img src="img/${fromJson[i]}.png" alt="" width="32px" height="32px">${fromJson[i]}` 
+				createLi.addEventListener('click', function addClick(e = event) {
+					if (count < 2 ) {
+						let id_click = e.target.id;
+					    let newItem = new Game(id_click)
+					    newItem.createImg();
+						count++;
+								}
+						else {
+							return
+						} 
+
+				})
+		
+				if (i < 11) {
+					takeUl[0].appendChild(createLi);
+				}
+				else if (i < 22) {
+					takeUl[1].appendChild(createLi);
+				}
+				else if (i < 33) {
+					takeUl[2].appendChild(createLi);
+				}
+		}	
+		}
+		else {
+			fromJson = count2;
+		}
+
+
+	function setStorage(name, arr) {
+
+		const toJson = JSON.stringify(arr)
+
+		console.log(toJson)
+
+		localStorage.setItem('array', toJson);
+		localStorage.setItem(name, name)
+
+	}
+
+
 	button.addEventListener('click', function() {
 		if (count === 2) { // Если в поле для создания 2 элемента
 			let currentItems = [], // массив в который будем записываем имена этих 2 элементов
@@ -36,15 +92,15 @@ window.onload = (function() {
 			for (let key in answers) { //перебираем обьект с ответами
 				if (key == `${first} + ${second}` ||
 					key == `${second} + ${first}`) { // Если ключ из обьекта равен строке из двух элементов
-					count = 0;   // Обнуляем счетчик	
-					for (let value of count2) {
+					count = 0;   // Обнуляем счетчик
+					for (let value of fromJson) { // Я устал писать комментарии(
 						if (value !== answers[key]) continue
 							else {
 									button.style.background = "#FF4500";
 									button.innerHTML = "You have this item"
 									setTimeout(function() {
 										button.style.background = "rgba(0,0,0,.4)"
-										button.innerHTML = 'Activate the magic circle'; // Я устал писать комменты(((
+										button.innerHTML = 'Activate the magic circle'; 
 									}, 1000)
 
 									while (craft.firstChild) {
@@ -54,7 +110,7 @@ window.onload = (function() {
 									return
 								}
 						}
-					count2.push(answers[key])
+					fromJson.push(answers[key])
 					let newLi = document.createElement('li'); //Создаем Li
 					newLi.setAttribute('id', answers[key]); //Даем созданному li id;
 					newLi.innerHTML = `<img src="img/${answers[key]}.png" alt="" width="32px" height="32px">${answers[key]}` 
@@ -69,15 +125,15 @@ window.onload = (function() {
 							return
 						}
 					})
-						
-					if (count2.length < 11) {	
+						console.log(fromJson)
+					if (fromJson.length < 12) {	
 						ingredientsUl[0].appendChild(newLi); // Добавляем созданный li в ul
 					}
-					else if (count2.length < 21) {
+					else if (fromJson.length < 23) {
 						ingredientsUl[1].appendChild(newLi); // Добавляем созданный li в ul
 						show2()
 					}
-					else if (count2.length < 32) {
+					else if (fromJson.length < 34) {
 						ingredientsUl[2].appendChild(newLi); // Добавляем созданный li в ul
 						show3()
 					}
@@ -91,6 +147,8 @@ window.onload = (function() {
 						button.style.background = "rgba(0,0,0,.4)"
 						button.innerHTML = 'Activate the magic circle';
 					}, 1000) //Через секунду возвращаем все на место
+						console.log(fromJson)
+						setStorage(answers[key], fromJson)
 						return
 					}
 
@@ -144,6 +202,7 @@ const answers = {
 class Game {
 	constructor(id) {
 		this.id = id;
+		
 	}
 
 	createImg() {
