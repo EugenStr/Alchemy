400dda028af77c6459ad6fb5fab67b321de4ac88
Eugen Str <asng99bk@yandex.ru>
2019-04-21 17:22:47 +0400

    cleaned code

--- script.js
+++ script.js
@@ -7,46 +7,46 @@ window.onload = (function() {
 		count = 0,
 		count2 = ['Water', 'Earth', 'Air', 'Fire'];
 
-
-		for (let i = 0; i < ingredients.length; i++) {
-			ingredients[i].addEventListener('click', function addClick(e = event) {
-				if (count < 2 ) {
-					let id_click = e.target.id;
-		         	let newItem = new Game(id_click)
-		         	newItem.createImg();
-		         	count++;
-				}
-				else {
-					return
-				}
+	for (let i = 0; i < ingredients.length; i++) {
+		ingredients[i].addEventListener('click', function addClick(e = event) {
+			if (count < 2 ) {
+				let id_click = e.target.id;
+		       	let newItem = new Game(id_click)
+		       	newItem.createImg();
+	         	count++;
+			}
+			else {
+				return
+			}
 			
 		})
 	}
-		button.addEventListener('click', function() {
-			
-			if (count === 2) { // Если в поле для создания 2 элемента
-				let currentItems = []; // массив в который будем записываем имена этих 2 элементов
-				let childs = [...craft.childNodes]; // Создаем массив из двух элементов стола
-				for (let value of childs ) { // Перебираем этот массив
-					currentItems.push(value.id) // Добавляем ID элементов в новый массив
-				}
-				if (currentItems[0] === undefined) { //Если в массиве первый элемент undefined
+
+	button.addEventListener('click', function() {
+		if (count === 2) { // Если в поле для создания 2 элемента
+			let currentItems = [], // массив в который будем записываем имена этих 2 элементов
+				childs = [...craft.childNodes]; // Создаем массив из двух элементов стола
+			for (let value of childs ) { // Перебираем этот массив
+				currentItems.push(value.id) // Добавляем ID элементов в новый массив
+			}
+			if (currentItems[0] === undefined) { //Если в массиве первый элемент undefined
 					currentItems.reverse().pop() //Перевариваем этот массив и удаляем последний элемент
 				}
-				let [first, second] = currentItems // Создаем переменные из этого массива
-				for (let key in answers) { //перебираем обьект с ответами
-					if (key == `${first} + ${second}` ||
-						key == `${second} + ${first}`) { // Если ключ из обьекта равен строке из двух элементов
-						count = 0;   // Обнуляем счетчик	
-						for (let value of count2) {
-							if (value !== answers[key]) continue
-								else {
+			let [first, second] = currentItems; // Создаем переменные из этого массива
+			for (let key in answers) { //перебираем обьект с ответами
+				if (key == `${first} + ${second}` ||
+					key == `${second} + ${first}`) { // Если ключ из обьекта равен строке из двух элементов
+					count = 0;   // Обнуляем счетчик	
+					for (let value of count2) {
+						if (value !== answers[key]) continue
+							else {
 									button.style.background = "#FF4500";
 									button.innerHTML = "You have this item"
 									setTimeout(function() {
-									button.style.background = "rgba(0,0,0,.4)"
-									button.innerHTML = 'Activate the magic circle';
+										button.style.background = "rgba(0,0,0,.4)"
+										button.innerHTML = 'Activate the magic circle'; // Я устал писать комменты(((
 									}, 1000)
+
 									while (craft.firstChild) {
 			   						 craft.removeChild(craft.firstChild);
 									}
@@ -54,48 +54,46 @@ window.onload = (function() {
 									return
 								}
 						}
-
-						count2.push(answers[key])
-						let newLi = document.createElement('li'); //Создаем Li
-						newLi.setAttribute('id', answers[key]); //Даем созданному li id;
-						newLi.innerHTML = `<img src="img/${answers[key]}.png" alt="" width="32px" height="32px">${answers[key]}` 
-						newLi.addEventListener('click', function addClick(e = event) {
-								if (count < 2 ) {
-									let id_click = e.target.id;
-						         	let newItem = new Game(id_click)
-						         	newItem.createImg();
-						         	count++;
+					count2.push(answers[key])
+					let newLi = document.createElement('li'); //Создаем Li
+					newLi.setAttribute('id', answers[key]); //Даем созданному li id;
+					newLi.innerHTML = `<img src="img/${answers[key]}.png" alt="" width="32px" height="32px">${answers[key]}` 
+					newLi.addEventListener('click', function addClick(e = event) {
+						if (count < 2 ) {
+							let id_click = e.target.id;
+					       	let newItem = new Game(id_click)
+					         	newItem.createImg();
+						         count++;
 								}
-								else {
-									return
-								}
-						})
-						if (count2.length < 11) {	
-							ingredientsUl[0].appendChild(newLi); // Добавляем созданный li в ul
+						else {
+							return
 						}
-						else if (count2.length < 21) {
-							ingredientsUl[1].appendChild(newLi); // Добавляем созданный li в ul
-							show2()
-						}
-						else if (count2.length < 32) {
-							ingredientsUl[2].appendChild(newLi); // Добавляем созданный li в ul
-							show3()
-						}
+					})
+						
+					if (count2.length < 11) {	
+						ingredientsUl[0].appendChild(newLi); // Добавляем созданный li в ul
+					}
+					else if (count2.length < 21) {
+						ingredientsUl[1].appendChild(newLi); // Добавляем созданный li в ul
+						show2()
+					}
+					else if (count2.length < 32) {
+						ingredientsUl[2].appendChild(newLi); // Добавляем созданный li в ul
+						show3()
+					}
 
-						while (craft.firstChild) {
-   							craft.removeChild(craft.firstChild); // Чистим ul с элементами
-						}
-						button.style.background = "green"; //Изменяем фон кнопки на зеленый
-						button.innerHTML = `Created ${answers[key]}` //Изменяем текст кнопки
-						setTimeout(function() {
-							button.style.background = "rgba(0,0,0,.4)"
-							button.innerHTML = 'Activate the magic circle';
-						}, 1000) //Через секунду возвращаем все на место
-
-
-
+					while (craft.firstChild) {
+   						craft.removeChild(craft.firstChild); // Чистим ul с элементами
+					}
+					button.style.background = "green"; //Изменяем фон кнопки на зеленый
+					button.innerHTML = `Created ${answers[key]}` //Изменяем текст кнопки
+					setTimeout(function() {
+						button.style.background = "rgba(0,0,0,.4)"
+						button.innerHTML = 'Activate the magic circle';
+					}, 1000) //Через секунду возвращаем все на место
 						return
 					}
+
 					else {
 						button.style.background = "red";
 						button.innerHTML = "Ooops"
@@ -105,8 +103,8 @@ window.onload = (function() {
 						}, 1000)
 						while (craft.firstChild) {
    							 craft.removeChild(craft.firstChild);
-							}
-							count = 0;
+						}
+						count = 0;
 					}
 				}
 
@@ -146,7 +144,6 @@ const answers = {
 class Game {
 	constructor(id) {
 		this.id = id;
-		this.count++;
 	}
 
 	createImg() {
